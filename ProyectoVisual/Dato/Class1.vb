af4aea4aff3b78639b97f1dc7bc5ba4ffa0c33dd
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-23 12:37:05 -0300

    Datos 1.6

--- ProyectoVisual/Dato/Class1.vb
+++ ProyectoVisual/Dato/Class1.vb
@@ -1,25 +1,3 @@
-﻿Imports System.Data.Odbc
-Imports System.Data.SqlClient
-Public Class ABM
-    Public connection As Conexion = New Conexion
+﻿Public Class Class1
 
-
-    Function AgregaCliente(ByVal txt As String) As String
-        Try
-            Dim comand As OdbcCommand = New OdbcCommand("{call prueba (?)}", connection.cn)
-            comand.CommandType = CommandType.StoredProcedure
-            comand.Parameters.AddWithValue("Descripcion", txt)
-            connection.cn.Open()
-            comand.ExecuteNonQuery()
-            connection.cn.Close()
-            MsgBox("El texto se argrego bien")
-        Catch ex As OdbcException
-            Return False
-        Finally
-
-            connection.cn.Close()
-        End Try
-
-        Return True
-    End Function
 End Class
