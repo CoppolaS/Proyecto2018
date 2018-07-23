f351b69a3edb20ea27f281870276700dc156b4b6
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-23 10:50:26 -0300

    Datos 1.3

--- ProyectoVisual/ProyectoVisual/My Project/Application.Designer.vb
+++ ProyectoVisual/ProyectoVisual/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto.Login
+            Me.MainForm = Global.Proyecto.Prueba
         End Sub
     End Class
 End Namespace
