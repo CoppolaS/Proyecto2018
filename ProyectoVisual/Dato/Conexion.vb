2b670e411b75180060fbf3fa7c9a918aec4b1823
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-23 10:23:03 -0300

    Datos 1.1

--- ProyectoVisual/Dato/Conexion.vb
+++ ProyectoVisual/Dato/Conexion.vb
@@ -1,3 +1,16 @@
-﻿Public Class Conexion
+﻿Imports System.Data
+Imports System.Data.Odbc
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
 
+Public Class Conexion
+    Public cn As OdbcConnection
+    Sub New()
+        cn = New OdbcConnection("UID=jpacheco" & _
+            ";PWD=142857" & _
+            ";DATABASE=bdd_proyecto" & _
+            ";SERVER=192.168.0.201" & _
+            ";DRIVER={MySQL ODBC 5.3 ANSI Driver};")
+    End Sub
 End Class
+
