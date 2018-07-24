51790e5af2cab2f26366baefe96329ae465aa179
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-24 16:32:49 -0300

    Cambios

--- ProyectoVisual/ProyectoVisual/Prueba.vb
+++ ProyectoVisual/ProyectoVisual/Prueba.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Prueba
     Dim txt As String
-    Dim d As Dato.
+    Dim d As Datos.ABM
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-
+        txt = Me.TextBox1.Text
+        d.AgregaCliente(txt)
     End Sub
 End Class
