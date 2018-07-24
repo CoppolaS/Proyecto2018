16dc87d0e8a183941a854976d1432125218aec1a
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-24 10:21:09 -0300

    Datos 1.9

--- ProyectoVisual/ProyectoVisual/Prueba.vb
+++ ProyectoVisual/ProyectoVisual/Prueba.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Prueba
+    Dim txt As String
+    Dim d As Dato.
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
 
+    End Sub
 End Class
