2b35ff8820c5ab197b2b7334a73a7f64b2e94a6a
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-24 10:04:27 -0300

    Datos 1.7

--- ProyectoVisual/Dato/Conexion.vb
+++ ProyectoVisual/Dato/Conexion.vb
@@ -6,8 +6,8 @@ Imports System.Data.SqlClient
 Public Class Conexion
     Public cn As OdbcConnection
     Sub New()
-        cn = New OdbcConnection("UID=jpacheco" & _
-            ";PWD=142857" & _
+        cn = New OdbcConnection("UID=programa" & _
+            ";PWD=12345678" & _
             ";DATABASE=proyecto_s.i.ges.vi" & _
             ";SERVER=192.168.0.201" & _
             ";DRIVER={MySQL ODBC 5.3 ANSI Driver};")
