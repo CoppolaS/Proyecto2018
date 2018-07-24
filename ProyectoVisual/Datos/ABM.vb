b80fbc35eb98dc3f3fa58ef27109d6148fa1909f
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-24 16:30:48 -0300

    Probar conexion

---
+++ ProyectoVisual/Datos/ABM.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Public Class ABM
+    Public connection As Conexion = New Conexion
+    Function AgregaCliente(ByVal txt As String)
+        Try
+            Dim comand As OdbcCommand = New OdbcCommand("{call prueba (?)}", connection.cn)
+            comand.CommandType = CommandType.StoredProcedure
+            comand.Parameters.AddWithValue("@Descripcion", txt)
+            connection.cn.Open()
+            comand.ExecuteNonQuery()
+            connection.cn.Close()
+            MsgBox("El cliente se agrego correctamente")
+        Catch ex As OdbcException
+            Return False
+        Finally
+
+            connection.cn.Close()
+        End Try
+
+        Return True
+    End Function
+
+End Class
