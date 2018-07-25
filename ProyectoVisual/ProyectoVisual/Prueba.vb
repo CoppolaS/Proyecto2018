ad392fcb1a3ded106f60ca4b1d04f6a4139260de
pache08 <36816088+pache08@users.noreply.github.com>
2018-07-25 13:07:32 -0300

    Datos 2.3

--- ProyectoVisual/ProyectoVisual/Prueba.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Prueba
-    Dim txt As String
-    Dim d As New Datos.ABM
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        txt = Me.TextBox1.Text
-        d.AgregaCliente(txt)
-    End Sub
-End Class
