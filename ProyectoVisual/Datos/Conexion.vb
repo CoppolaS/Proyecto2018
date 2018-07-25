ada789c752935cdb67172eb77abedcbb1d5fbe95
frankete333 <36253280+frankete333@users.noreply.github.com>
2018-07-25 13:21:59 -0300

    proyecto V1.0

---
+++ ProyectoVisual/Datos/Conexion.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data
+Imports System.Data.Odbc
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+
+Public Class Conexion
+    Public cn As OdbcConnection
+    Sub New()
+        cn = New OdbcConnection("UID=programa" & _
+            ";PWD=12345678" & _
+            ";DATABASE=proyecto_s.i.ges.vi" & _
+            ";SERVER=192.168.0.201" & _
+            ";DRIVER={MySQL ODBC 5.3 ANSI Driver};")
+    End Sub
+End Class
