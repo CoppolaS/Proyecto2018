d7d7d2f25a1d5c5e7c4f2a13d91254864d726dc3
Tiago S. Cóppola <tiagosalvattore@hotmail.com>
2018-08-05 16:36:04 -0300

    l

--- Proyecto/Negocio/Verificar/VerificarEmpresa.vb
+++ Proyecto/Negocio/Verificar/VerificarEmpresa.vb
@@ -3,12 +3,9 @@
 Public Class VerificarEmpresa
     Dim DatosE As New Datos.DatosEmpresa
 
+    'sucursales
     Public Function ValidoListaSucursales() As ArrayList
         Return DatosE.ListaSucursales
-    End Function
-
-    Public Function ValidoListaFuncionarios(Optional ByVal ID As Integer = 0) As ArrayList
-        Return DatosE.ListaFuncionarios(ID)
     End Function
 
     Public Function ValidoIngresoSucursales(ByVal encapsuladora As Encapsuladoras.Sucursales)
@@ -24,6 +21,21 @@ Public Class VerificarEmpresa
     Public Function ValidoModificarSucursales(ByVal encapsuladora As Encapsuladoras.Sucursales)
         DatosE.ModificoSucursal(encapsuladora)
         Return Nothing
+    End Function
+
+    'Public Function CargarTabla(ByRef tabla As DataTable) As DataSet
+    '    Dim ds As New DataSet
+    '    For Each itemSucursal As Encapsuladoras.Sucursales In ValidoListaSucursales()
+    '        If itemSucursal.EliminadoSucursal = False Then
+    '            ds.(itemSucursal.IDSucursal, itemSucursal.NombreSucursal, itemSucursal.DireccionSucursal)
+    '        End If
+    '    Next
+    '    Tabla1.DataGridView1.ClearSelection()
+    'End Function
+
+    'funcionarios
+    Public Function ValidoListaFuncionarios(Optional ByVal ID As Integer = 0) As ArrayList
+        Return DatosE.ListaFuncionarios(ID)
     End Function
 
 End Class
