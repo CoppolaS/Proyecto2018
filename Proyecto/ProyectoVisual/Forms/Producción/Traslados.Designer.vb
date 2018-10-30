0e33243d013e1216b40c020d2b17af4c4711c68a
Tiago S. Cóppola <tiagosalvattore@hotmail.com>
2018-10-30 09:49:46 -0300

    1 día mas

---
+++ Proyecto/ProyectoVisual/Forms/Producción/Traslados.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Traslados
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Menu1 = New Proyecto.Menu()
+        Me.SuspendLayout()
+        '
+        'Menu1
+        '
+        Me.Menu1.Location = New System.Drawing.Point(12, 12)
+        Me.Menu1.Name = "Menu1"
+        Me.Menu1.Size = New System.Drawing.Size(1240, 32)
+        Me.Menu1.TabIndex = 0
+        '
+        'Traslados
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.Menu1)
+        Me.MaximizeBox = False
+        Me.Name = "Traslados"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Traslados"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Menu1 As Proyecto.Menu
+End Class
---
+++ Proyecto/ProyectoVisual/Forms/Producción/Traslados.vb
@@ -0,0 +1,3 @@
+﻿Public Class Traslados
+
+End Class
